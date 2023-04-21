99cce10da3e6ee24bce4d860c8c957d7cd60ae8f
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-04-21 19:22:19 +0900

    ルームチャット用APIを追加

---
+++ controllers/chat/room_chat_controller.go
@@ -0,0 +1,52 @@
+package chat
+
+import (
+	"log"
+	"net/http"
+	"github.com/labstack/echo"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	roomChatUpgrader = websocket.Upgrader{
+		ReadBufferSize: 1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+
+	rooms = make(map[string]map[*websocket.Conn]bool)
+)
+
+func RoomChatHandler(c echo.Context) error {
+	room := c.Param("room")
+	conn, err := roomChatUpgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	if rooms[room] == nil {
+		rooms[room] = make(map[*websocket.Conn]bool)
+	}
+
+	rooms[room][conn] = true
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			delete(rooms[room], conn)
+			return err
+		}
+
+		message := string(p)
+
+		for client := range rooms[room] {
+			err := client.WriteMessage(messageType, []byte(message))
+			if err != nil {
+				log.Println(err)
+				delete(rooms[room], client)
+			}
+		}
+	}
+}
--- controllers/router.go
+++ controllers/router.go
@@ -10,6 +10,7 @@ func Router() *echo.Echo {
 	e := echo.New()
 
 	e.GET("/open_chat", chat.OpenChatHandler)
+	e.GET("/room_chat/:room", chat.RoomChatHandler)
 
 	e.Start(":8000")
 
